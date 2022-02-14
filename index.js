4f52057c7e9a1b927603fb2480115dda1563a3c1
NicMortelliti <npmortelliti@gmail.com>
2022-02-14 14:50:45 -0800

    commit tree e0e770d5a94ee8822148d3867aaeb5b670a989a8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(data) {
+  // Arrow function to 'find' a result of "W" in the array of objects
+  const objectMatch = data.find(object => object.result === "W");
+
+  // If objectMatch is 'Truthy' (i.e. it is NOT 'undefined'), return
+  // the value of the 'year' key. Otherwise, return 'undefined'.
+  return objectMatch ? objectMatch.year : undefined;
+}
